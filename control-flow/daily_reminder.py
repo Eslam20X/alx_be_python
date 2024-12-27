72ba10a9403119bcc4f23938d9e7160f45bdce52
Eslam20X <elmoasry8@yahoo.com>
2024-12-27 23:15:18 +0200

    Reminder B

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ").title().strip()
 priority = input("Priority (high/medium/low): ").lower().strip()
-time = input("Is it time-bound? (yes/no): ").lower().strip()
+time_bound = input("Is it time-bound? (yes/no): ").lower().strip()
 
 reminder = f"'{task}' is a {priority} priority task,"
 
@@ -25,19 +25,19 @@ reminder = f"'{task}' is a {priority} priority task,"
 
 match priority:
     case "high":
-        if time == "yes":
+        if time_bound == "yes":
             print(reminder + " task that requires immediate attention today!")
         else:
             print(reminder + " should start it once you completely finish high priority time bounded tasks")
     
     case "medium":
-        if time == "yes":
+        if time_bound == "yes":
             print(reminder + " ,Start after high priority Non-Time-Bound tasks")
         else:
             print(reminder + " ,Start after low priority tasks")
     
     case "low":
-        if time == "yes":
+        if time_bound == "yes":
             print(reminder + " As it has a deadline it should be done before mediume Non-Time-Bound tasks")
         else:
             print(reminder + " Consider completing it when you have free time.")
