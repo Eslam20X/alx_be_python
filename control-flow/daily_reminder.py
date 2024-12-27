a791196ac948ea6425b3d69e8b8c223f0c0b1b17
Eslam20X <elmoasry8@yahoo.com>
2024-12-27 22:50:16 +0200

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,1 +1,22 @@
+task = input("Enter your task: ").title().strip()
+priority = input("Priority (high/medium/low): ").lower().strip()
+time = input("Is it time-bound? (yes/no): ").lower().strip()
 
+reminder = f"'{task}' is a {priority} priority task,"
+
+match time:
+    case "yes":
+        if priority == "high":
+            print(reminder + " task that requires immediate attention today!")
+        elif priority == "medium":
+            print(reminder + " ,Start after high priority Non-Time-Bound tasks")
+        elif priority == "low":
+            print(reminder + " As it has a deadline it should be done before mediume Non-Time-Bound tasks")
+        
+    case "no":
+        if priority == "high":
+            print(reminder + " should start it once you completely finish high priority tasks")
+        elif priority == "medium":
+            print(reminder + " , start after low priority tasks")
+        elif priority == "low":
+            print(reminder + " Consider completing it when you have free time.")
--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,6 @@
 size = int(input("Enter the size of the pattern: "))
 check = 0
+
 while check != size:
     for j in range(size):
         print("*", end="")
